b880640d379ddbf30afc44ab430f73a056140856
fushichang <fushichang@liqiang365.com>
2019-12-19 14:18:17 +0800

    d3 测试demo

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 let pageMethod = require('./utils/getPages.js');
 let pages = pageMethod.pages();
+
+const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin');
+
+
 module.exports = {
   pages
 }
